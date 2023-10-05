2a6bfa6a6ce85e93468b4068f38abb151ccd6424
RhoSeungA <sskclara@naver.com>
2023-10-05 17:47:08 +0900

    initialized

---
+++ src/main/java/com/example/dssw/DsswApplication.java
@@ -0,0 +1,13 @@
+package com.example.dssw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dssw/DsswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dssw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
