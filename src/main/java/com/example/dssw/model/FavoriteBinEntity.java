ce58a6af370e0250780c561f1700112d7b2d176e
mk020 <mk02081@naver.com>
2023-10-10 20:54:09 +0900

    DB 테이블 생성

---
+++ src/main/java/com/example/dssw/model/FavoriteBinEntity.java
@@ -0,0 +1,29 @@
+package com.example.dssw.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="favorite_bin")
+public class FavoriteBinEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE, targetEntity = UserEntity.class)
+    @JoinColumn(referencedColumnName = "id", name = "userid", nullable = false)
+    UserEntity user; // 유저 id
+
+    @Column(nullable=false)
+    private Long bin_id; // 휴지통 id
+
+    @Column(nullable = false, length = 10)
+    private String bin_type; // 가로휴지통인지 재활용정거장인지
+}
---
+++ src/main/java/com/example/dssw/model/ReportDeclarationEntity.java
@@ -0,0 +1,45 @@
+package com.example.dssw.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="report_declaration")
+public class ReportDeclarationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE, targetEntity = UserEntity.class)
+    @JoinColumn(referencedColumnName = "id", name = "userid", nullable = false)
+    UserEntity user; // 유저 id
+
+    @Column(nullable=false, length = 45)
+    private String address;
+
+    @Column(nullable=false, length = 45)
+    private String detail_location;
+
+    @Column(nullable=false, length = 200)
+    private String image_url;
+
+    @Column(columnDefinition = "TEXT")
+    private String context;
+
+    @Column(nullable=false, length = 10)
+    private String type; // 신고 종류
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    @Column(nullable = false)
+    private LocalDateTime date; // 업로드 날짜 시간 분 초
+}
