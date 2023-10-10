4c1a7f747646c32c627d2f5b7ddcfb626e7dfc65
RhoSeungA <sskclara@naver.com>
2023-10-10 15:41:10 +0900

    로그인 기능

---
+++ src/main/java/com/example/dssw/model/UserEntity.java
@@ -0,0 +1,41 @@
+package com.example.dssw.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import javax.*;
+//import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false,length = 10)
+    private String userid;
+
+    @Column(nullable=false,length = 10)
+    private String nickname; // 아이디와 같은 기능
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(nullable=false)
+    private String phonenum;
+
+    @Column(nullable = false)
+    private  int reward;
+
+    @Column(nullable = false)
+    private  boolean admin;
+
+}
