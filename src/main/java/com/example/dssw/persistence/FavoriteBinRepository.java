958148082d675a7a748817ec03b153efa9a7d6f9
RhoSeungA <sskclara@naver.com>
2023-11-09 13:07:27 +0900

    마이페이지 - 즐겨찾기

--- src/main/java/com/example/dssw/persistence/FavoriteBinRepository.java
+++ src/main/java/com/example/dssw/persistence/FavoriteBinRepository.java
@@ -4,8 +4,10 @@ import com.example.dssw.model.FavoriteBinEntity;
 import com.example.dssw.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteBinRepository extends JpaRepository<FavoriteBinEntity,Long> {
     Optional<FavoriteBinEntity> findByUserAndBinIdAndBinType(UserEntity user, Long binId, String binType);
+
 }
